ec892e4f4bb21647f552f1022ac1de0dc5fc9207
hunterlanier.eth <hlanier97@gmail.com>
2024-01-29 21:16:26 -0600

    init

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<script></script>
+
+<template>
+  <div class="h-screen w-screen bg-gray-900 flex justify-center items-center">
+    <h1 class="text-white">Welcome to Boiler Plate!</h1>
+  </div>
+</template>
